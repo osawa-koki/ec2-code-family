7e7a7b2b533602b600014928c61167ca34874321
osawa-koki <koki@osawa.dev>
2025-01-12 10:52:07 +0900

    EC2にSSHログインができるように

--- README.md
+++ README.md
@@ -6,6 +6,8 @@
 
 `.env.example`をコピーして`.env`ファイルを作成します。  
 中身を適切に設定してください。  
+また、公開鍵ペアを作成してください。  
+公開鍵のパスを`SSH_PUBLIC_KEY_MATERIAL_PATH`に設定してください。  
 
 DevContainerに入り、以下のコマンドを実行します。  
 ※ `~/.aws/credentials`にAWSの認証情報があることを前提とします。  
@@ -14,6 +16,14 @@ DevContainerに入り、以下のコマンドを実行します。
 cdk bootstrap
 cdk synth
 cdk deploy --require-approval never --all
+```
+
+SSHでEC2に接続するためには、以下のコマンドを実行してください。  
+
+```shell
+ssh -i <秘密鍵のパス> ec2-user@<EC2のパブリックIPアドレス>
+
+# ssh -i ~/.ssh/id_rsa ec2-user@<EC2のパブリックIPアドレス>
 ```
 
 ---
--- lib/compute/compute.ts
+++ lib/compute/compute.ts
@@ -1,3 +1,5 @@
+import * as fs from 'fs';
+import * as os from 'os';
 import * as cdk from 'aws-cdk-lib';
 import * as ec2 from 'aws-cdk-lib/aws-ec2';
 import { Construct } from 'constructs';
@@ -35,6 +37,17 @@ export default class ComputeStack extends cdk.Stack {
       ec2.Port.tcp(443),
       'Allow HTTPS traffic for CodeDeploy agent'
     );
+    securityGroup.addIngressRule(
+      ec2.Peer.anyIpv4(),
+      ec2.Port.tcp(22),
+      'Allow SSH access'
+    );
+
+    const publicKeyPath = process.env.SSH_PUBLIC_KEY_MATERIAL_PATH!.replace('~', os.homedir());
+    const keyPair = new ec2.KeyPair(this, 'MyKeyPair', {
+      keyPairName: `${stackName}-key-pair`,
+      publicKeyMaterial: fs.readFileSync(publicKeyPath, 'utf8'),
+    });
 
     const ec2Instance = new ec2.Instance(this, 'MyInstance', {
       vpc: props.vpc,
@@ -42,6 +55,7 @@ export default class ComputeStack extends cdk.Stack {
       instanceType: ec2.InstanceType.of(ec2.InstanceClass.T3, ec2.InstanceSize.NANO),
       machineImage: ec2.MachineImage.latestAmazonLinux2023(),
       securityGroup: securityGroup,
+      keyPair: keyPair,
     });
     cdk.Tags.of(ec2Instance).add('DeployGroup', deploymentGroupName);
 
